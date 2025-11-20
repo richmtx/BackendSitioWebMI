f641c711f34ef33caeb30a7b060b9e86e3b5b509
rickymtx <rich.mtx1205@gmail.com>
2025-11-20 15:03:52 -0600

    Commit

---
+++ Admin-Panel/src/app/pages/productividad-lgyac/lgacArticulos.service.ts
@@ -0,0 +1,41 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+export interface LgacArticulo {
+  id_articulo: number;
+  titulo: string;
+  descripcion: string;
+  fecha_publicacion: string;
+  url: string;
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LgacArticulosService {
+
+  private apiUrl = 'http://localhost:3000/lgac-articulos';
+
+  constructor(private http: HttpClient) {}
+
+  getArticulos(): Observable<LgacArticulo[]> {
+    return this.http.get<LgacArticulo[]>(this.apiUrl);
+  }
+
+  getArticulo(id: number): Observable<LgacArticulo> {
+    return this.http.get<LgacArticulo>(`${this.apiUrl}/${id}`);
+  }
+
+  crearArticulo(data: Partial<LgacArticulo>): Observable<LgacArticulo> {
+    return this.http.post<LgacArticulo>(this.apiUrl, data);
+  }
+
+  actualizarArticulo(id: number, data: Partial<LgacArticulo>): Observable<any> {
+    return this.http.put(`${this.apiUrl}/${id}`, data);
+  }
+
+  eliminarArticulo(id: number): Observable<any> {
+    return this.http.delete(`${this.apiUrl}/${id}`);
+  }
+}
--- Admin-Panel/src/app/pages/productividad-lgyac/productividad-lgyac.component.ts
+++ Admin-Panel/src/app/pages/productividad-lgyac/productividad-lgyac.component.ts
@@ -1,11 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { LgacArticulosService, LgacArticulo } from './lgacArticulos.service';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-productividad-lgyac',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, FormsModule],
   templateUrl: './productividad-lgyac.component.html',
-  styleUrl: './productividad-lgyac.component.css'
+  styleUrls: ['./productividad-lgyac.component.css'],
 })
-export class ProductividadLgyacComponent {
+export class ProductividadLgyacComponent implements OnInit {
 
+  articulos: LgacArticulo[] = [];
+
+  constructor(private lgacService: LgacArticulosService) { }
+
+  ngOnInit(): void {
+    this.cargarArticulos();
+  }
+
+  cargarArticulos(): void {
+    this.lgacService.getArticulos().subscribe({
+      next: (data) => {
+        this.articulos = data;
+      },
+      error: (err) => {
+        console.error('Error al obtener artículos:', err);
+      }
+    });
+  }
+
+  abrirArticulo(url: string): void {
+    console.log('URL del artículo:', url); // te mostrará la URL correcta
+    if (!url) return;
+
+    window.open(url, '_blank');
+  }
 }
