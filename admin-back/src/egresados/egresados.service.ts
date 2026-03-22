bdb165d34dc79a897fdec9cdc6f0d47de211c9c6
richmtx <martinezhernandezricardo06@gmail.com>
2026-03-22 14:10:52 -0600

    Commit

--- admin-back/src/egresados/egresados.service.ts
+++ admin-back/src/egresados/egresados.service.ts
@@ -3,24 +3,53 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Egresados } from './egresados.entity';
 
+import * as fs from 'fs';
+import * as path from 'path';
+
 @Injectable()
 export class EgresadosService {
   constructor(
     @InjectRepository(Egresados)
     private readonly egresadosRepository: Repository<Egresados>,
-  ) {}
+  ) { }
 
-  findAll(): Promise<Egresados[]> {
-    return this.egresadosRepository.find();
+  async findAll(): Promise<any[]> {
+    const egresados = await this.egresadosRepository.find();
+
+    return egresados.map((egresado) => {
+      if (egresado.imagen) {
+        const imagePath = path.join(process.cwd(), egresado.imagen);
+
+        if (fs.existsSync(imagePath)) {
+          const file = fs.readFileSync(imagePath);
+          const base64 = file.toString('base64');
+
+          egresado.imagen = `data:image/jpeg;base64,${base64}`;
+        }
+      }
+
+      return egresado;
+    });
   }
 
-  async findOne(id: number): Promise<Egresados> {
+  async findOne(id: number): Promise<any> {
     const egresado = await this.egresadosRepository.findOneBy({
       id_egresados: id,
     });
 
     if (!egresado) {
       throw new NotFoundException(`Egresado con ID ${id} no encontrado`);
+    }
+
+    if (egresado.imagen) {
+      const imagePath = path.join(process.cwd(), egresado.imagen);
+
+      if (fs.existsSync(imagePath)) {
+        const file = fs.readFileSync(imagePath);
+        const base64 = file.toString('base64');
+
+        egresado.imagen = `data:image/jpeg;base64,${base64}`;
+      }
     }
 
     return egresado;
